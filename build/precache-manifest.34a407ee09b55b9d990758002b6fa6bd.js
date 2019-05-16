2cf35276d1f26cbd739c799f827605eef22969b6
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-05-16 14:39:23 +0300

    chore(project): deploy project

---
+++ build/precache-manifest.34a407ee09b55b9d990758002b6fa6bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af4eee411224316a23fb5730248ccae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac41c8e6aeef3f41807e",
+    "url": "/static/css/2.436c5b30.chunk.css"
+  },
+  {
+    "revision": "ac41c8e6aeef3f41807e",
+    "url": "/static/js/2.41f3c67d.chunk.js"
+  },
+  {
+    "revision": "d266b4643dad31815876",
+    "url": "/static/js/main.d6cf8130.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4c396d3afb908f878b4835a616ba1af",
+    "url": "/static/media/close.c4c396d3.svg"
+  },
+  {
+    "revision": "ee68dcaf66328f22d739c201b0e1962f",
+    "url": "/static/media/logo.ee68dcaf.svg"
+  },
+  {
+    "revision": "18c2822ca7093b9171518a2aff5271dd",
+    "url": "/static/media/signout.18c2822c.svg"
+  }
+]);
